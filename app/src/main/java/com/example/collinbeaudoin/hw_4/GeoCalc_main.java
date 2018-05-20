323cc308d8c1f40d29a192af42d8959d269e0275
Collin Beaudoin <beaudoco@mail.gvsu.edu>
2018-05-19 20:13:32 -0400

    Set up of homework 4

---
+++ app/src/main/java/com/example/collinbeaudoin/hw_4/GeoCalc_main.java
@@ -0,0 +1,13 @@
+package com.example.collinbeaudoin.hw_4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GeoCalc_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_geo_calc_main);
+    }
+}
