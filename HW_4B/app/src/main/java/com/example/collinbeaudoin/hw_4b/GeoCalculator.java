204f250f9ad3a922a5b556e71d38cd1ebef34247
Collin Beaudoin <beaudoco@mail.gvsu.edu>
2018-05-21 22:05:12 -0400

    toolbar is up and running, all should be done on my end

--- HW_4B/app/src/main/java/com/example/collinbeaudoin/hw_4b/GeoCalculator.java
+++ HW_4B/app/src/main/java/com/example/collinbeaudoin/hw_4b/GeoCalculator.java
@@ -1,8 +1,13 @@
 package com.example.collinbeaudoin.hw_4b;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.design.widget.Snackbar;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
@@ -68,7 +73,19 @@ public class GeoCalculator extends AppCompatActivity {
         setContentView(R.layout.activity_geo_calculator);
         //needs to be fixed
         //relativeLayout = findViewById(R.id.relativeLayout);
+//        ignore
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                //this is the logic to go to the next pag uppon toolbar click
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
         // Main activity GUI elements.
         EditText p1LatTxt = (EditText) findViewById(R.id.p1LatTxt);
         final EditText p2LatTxt = (EditText) findViewById(R.id.p2LatTxt);
@@ -116,5 +133,27 @@ public class GeoCalculator extends AppCompatActivity {
             bearLbl.setText("Bearing:");
             distLbl.setText("Distance:");
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_geo_calculator, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
